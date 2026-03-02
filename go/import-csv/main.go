0f4e3306a6748a95d40b1cded0e847c323063891
Christopher Fujino <christopherfujino@gmail.com>
2026-03-01 22:45:55 -0800

    get working with sqlite

--- go/import-csv/main.go
+++ go/import-csv/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
+
+	"github.com/christopherfujino/chrislib-go/check"
+	_ "modernc.org/sqlite"
 )
 
 func main() {
@@ -12,4 +16,21 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("%v\n", os.Args)
+
+	db := check.Two(sql.Open("sqlite", "csv.db"))
+
+	check.Two(db.Query(`CREATE TABLE IF NOT EXISTS transactions (
+   id INTEGER PRIMARY KEY,
+   name TEXT NOT NULL
+);`))
+
+	// Insert
+	check.Two(db.Query(`INSERT INTO transactions (name) VALUES ('Foo');`))
+
+	var rows *sql.Rows = check.Two(db.Query("SELECT * FROM transactions;"))
+	var s1, s2 string
+	for rows.Next() {
+		check.One(rows.Scan(&s1, &s2))
+		fmt.Printf("%v %v\n", s1, s2)
+	}
 }
